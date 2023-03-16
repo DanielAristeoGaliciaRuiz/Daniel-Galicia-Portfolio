3ce2a218c35d98995a2fe4b626c40831627b4175
Daniel Galicia <danielaristeogaliciaruiz@gmail.com>
2023-03-16 15:31:23 -0600

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
-import LeftBar from './components/leftbar/LeftBar'
+// import LeftBar from './components/leftbar/LeftBar'
 import MainProyects from './components/mainProyects/MainProyects'
 import Navbar from './components/navbar/Navbar'
 import OtherProyects from './components/otherProyects/OtherProyects'
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
    <Navbar/>
    <div style={{display:'flex', justifyContent:'space-between'}}>
-   <LeftBar/>
+   {/* <LeftBar/> */}
    <div className='home'>
    <About/>
    <MainProyects/>
