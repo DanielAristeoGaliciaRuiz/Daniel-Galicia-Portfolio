e1e8e0505c6c372436a9525a30f08550c615c2ba
DanielAristeoGaliciaRuiz <116518743+DanielAristeoGaliciaRuiz@users.noreply.github.com>
2025-08-08 20:26:26 -0600

    Update MainProyects.jsx

--- src/components/mainProyects/MainProyects.jsx
+++ src/components/mainProyects/MainProyects.jsx
@@ -44,23 +44,21 @@ const MainProyects = () => {
             </div>
             <div  className='descrition-container'>
                 <div data-aos='fade-up' className='favorite-proyects'>Proyecto Favorito</div>
-                <div data-aos='fade-up' className='name-proyect'> E-commerce</div>
-                <div data-aos='fade-up' className='description-proyect'>Aplicación creada utilizando una API de academlo.
-                 Para el manejo de estados  globales se utilizó Redux-Toolkit y para el manejo de las rutas y rutas protegidas se utilizó React-Router-Dom.   </div>
+                <div data-aos='fade-up' className='name-proyect'> SeaSoul-Café</div>
+                <div data-aos='fade-up' className='description-proyect'>Aplicación web para promocionar una cafetería en la costa Oaxaqueña.
+                 Uso de React como framework principal. En proceso de conexión con su api para la gestión de un e-comerce.  </div>
 
                 <div className='tecnologys-used'>
                     <p data-aos='fade-up'>JavaScript</p>
                     <p data-aos='fade-up'>ReactJs</p>
-                    <p data-aos='fade-up'>Redux-toolkit</p>
-                    <p data-aos='fade-up'>React-router-dom</p>
                 </div>
                 
                 <div className='icons-container'>
-                <a data-aos='fade-up' href="https://github.com/DanielAristeoGaliciaRuiz/e-commerce" target='_blank'>
+                <a data-aos='fade-up' href="https://github.com/DanielAristeoGaliciaRuiz/seaSoulCoffe" target='_blank'>
                 <i className='bx bxl-github'></i>
                 </a>
 
-                <a data-aos='fade-up' href="https://e-commerce-reactjs-redux.netlify.app/" target='_blank'>
+                <a data-aos='fade-up' href="https://seasoulcoffe.netlify.app/" target='_blank'>
                 <i className='bx bx-link-external'></i>  
                 </a>
                 </div>
@@ -101,22 +99,22 @@ const MainProyects = () => {
             <div className='imgProyect-container tres'></div>
             <div className='descrition-container'>
                 <div data-aos='fade-up' className='favorite-proyects'>Proyecto Favorito</div>
-                <div data-aos='fade-up' className='name-proyect'> CRUD Users</div>
-                <div data-aos='fade-up' className='description-proyect'>Aplicación cuyo principal objetivo es realizar las acciones básicas de un CRUD. Realizando algunas modificaciones se puede adaptar para crear una to-do list.  </div>
+                <div data-aos='fade-up' className='name-proyect'> AxM-Consulting</div>
+                <div data-aos='fade-up' className='description-proyect'>Landing page para la agencia de consultoría de Actuarios por México</div>
 
                 <div className='tecnologys-used'>
-                    <p data-aos='fade-up'>JavaScript</p>
-                    <p data-aos='fade-up'>ReactJs</p>
+                    <p data-aos='fade-up'>HTML</p>
+                    <p data-aos='fade-up'>CSS</p>
                     <p data-aos='fade-up'></p>
                     <p data-aos='fade-up'></p>
                 </div>
                 
                 <div className='icons-container'>
-                <a data-aos='fade-up' href="https://github.com/DanielAristeoGaliciaRuiz/crud-user" target='_blank'>
+                <a data-aos='fade-up' href="https://github.com/DanielAristeoGaliciaRuiz/AxM-Consulting" target='_blank'>
                 <i className='bx bxl-github'></i>
                 </a>
 
-                <a data-aos='fade-up' href="https://crud-user-react.netlify.app" target='_blank'>
+                <a data-aos='fade-up' href="https://axmconsulting.netlify.app/" target='_blank'>
                 <i className='bx bx-link-external'></i>  
                 </a>  
                 </div>
